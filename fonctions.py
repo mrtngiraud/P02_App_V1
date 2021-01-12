85989575ad46bad6a0237f2528f4d981c8429f14
Martin Giraud <mrtngiraud@gmail.com>
2021-01-12 10:14:47 +0100

    ok fonction chiffre()

--- fonctions.py
+++ fonctions.py
@@ -1,12 +1,24 @@
-#enregistrement des images:
-
 import requests
+from bs4 import BeautifulSoup
 
+#enregistrer des images:
+'''
+a = url de la page livre
+b = titre du livre
+'''
 def saveImage(a,b):
     response = requests.get(a)
     if response.ok:
         with open('export/images/{}.jpg'.format(b), 'wb') as imageFile:
             imageFile.write(response.content)
+
+#convertir nombre en lettre vers nombre en chiffre (de 1 à 5)
+'''
+a = chiffre écrit en lettre
+'''
+def chiffre(a):
+    dico = {'One':'1','Two':'2','Three':'3','Four':'4','Five':'5'}
+    return dico.get(a)
 
 
 
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 import os
 import re
+from fonctions import chiffre
+from fonctions import saveImage
 
 print("Début d'extraction des données")
 
@@ -11,7 +13,7 @@ os.mkdir('export/images/')
 with open('export/'+'data.csv','a+',newline='') as csvFile:
     csvFile.write('product_page_url,universal_ product_code (upc),title,price_including_tax,price_excluding_tax,number_available,product_description,category,review_rating,image_url\n')
 
-    for i in range(4):
+    for i in range(2):
         url = 'http://books.toscrape.com/catalogue/page-{}.html'.format(i)
         response = requests.get(url)
 
@@ -19,8 +21,7 @@ with open('export/'+'data.csv','a+',newline='') as csvFile:
             soup = BeautifulSoup(response.text, 'html.parser')
             articles = soup.findAll('h3')
             for h3 in articles:
-                a = h3.find('a')
-                link = a['href']
+                link = h3.find('a')['href']
                 urlLinks = 'http://books.toscrape.com/catalogue/{}'.format(link)
                 response = requests.get(urlLinks)
 
@@ -38,21 +39,10 @@ with open('export/'+'data.csv','a+',newline='') as csvFile:
                     title = soup.find('h1').text.replace(',','').replace('/','')
                     description = soup.findAll('p')[3].text.replace(',','').replace('"','')
                     category = soup.findAll('a')[3].text
-                    review = soup.findAll('p')[2]['class'][1]
-                    if review == 'One':
-                        review = '1'
-                    if review == 'Two':
-                        review = '2'
-                    if review == 'Three':
-                        review = '3'
-                    if review == 'Four':
-                        review = '4'
-                    if review == 'Five':
-                        review = '5'
+                    review = chiffre(soup.findAll('p')[2]['class'][1])
                     imageLinks = soup.find('img')['src'].replace('../../','http://books.toscrape.com/')
-                    csvFile.write(urlLinks + ',' + code + ',' + title + ',' + priceNet + ',' + priceBrut + ',' + stock + ',' + description + ',' + category + ',' + review + ',' + imageLinks + '\n')
 
-                    from fonctions import saveImage
+                    csvFile.write(urlLinks + ',' + code + ',' + title + ',' + priceNet + ',' + priceBrut + ',' + stock + ',' + description + ',' + category + ',' + review + ',' + imageLinks + '\n')
                     saveImage(imageLinks,title)
 
 print("Fin d'extraction des données")
