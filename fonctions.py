e8065ac459765a83f5e606c9ab82153444dc3f44
Martin Giraud <mrtngiraud@gmail.com>
2021-01-12 19:44:15 +0100

    Dernières corrections ok

--- fonctions.py
+++ fonctions.py
@@ -1,57 +1,41 @@
+# Import des paquets Python:
 import requests
 
-#renvoyer l'url d'une page du site
+# Fonction1/ Renvoyer l'url d'une page du site:
 '''
 i = numéro d'une page du site
 '''
-def pageSite(i):
+def pagesite(i):
     return 'http://books.toscrape.com/catalogue/page-{}.html'.format(i)
 
-#renvoyer l'url d'une page d'un livre
-'''
-a = balise head
-b = 'balise body'
-c = 'class contenant le lien hypertexte'
-'''
-def pageLivre(a,b,c):
-        link = a.find(b)[c]
-        return 'http://books.toscrape.com/catalogue/{}'.format(link)
 
-#enregistrer une image
+# Fonction2/ Renvoyer l'url d'une page d'un livre:
 '''
-a = url de la page livre
+a = balise head de b
+b = 'balise body de c'
+c = 'class de c du lien hypertexte'
+'''
+def pagelivre(a, b, c):
+    link = a.find(b)[c]
+    return 'http://books.toscrape.com/catalogue/{}'.format(link)
+
+
+# Fonction3/ Convertir nombre lettre en nombre chiffre (de 1 à 5):
+'''
+a = nombre lettre
+'''
+def chiffre(a):
+    dico = {'One': '1', 'Two': '2', 'Three': '3', 'Four': '4', 'Five': '5'}
+    return dico.get(a)
+
+
+# Fonction4/ Enregistrer une image:
+'''
+a = url de la page d'un livre
 b = titre du livre
 '''
-def saveImage(a,b):
+def saveimage(a, b):
     response = requests.get(a)
     if response.ok:
         with open('export/images/{}.jpg'.format(b), 'wb') as imageFile:
             imageFile.write(response.content)
-
-#convertir nombre lettre en nombre chiffre (de 1 à 5)
-'''
-a = nombre en lettre
-'''
-def chiffre(a):
-    dico = {'One':'1','Two':'2','Three':'3','Four':'4','Five':'5'}
-    return dico.get(a)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- main.py
+++ main.py
@@ -1,44 +1,60 @@
-import requests
+# Import des paquets Python:
 from bs4 import BeautifulSoup
 import os
 import re
+
+# Import des fonctions:
 from fonctions import *
 
 print("Début d'extraction des données")
+
+# Création des dossiers export/ et export/images/:
 os.mkdir('export/')
 os.mkdir('export/images/')
 
-with open('export/'+'data.csv','a+',newline='') as csvFile:
-    csvFile.write('product_page_url,universal_ product_code (upc),title,price_including_tax,price_excluding_tax,number_available,product_description,category,review_rating,image_url\n')
+# Ouverture d'un fichier data.csv et écriture des en-têtes de colonnes:
+with open('export/'+'data.csv', 'a+', newline='') as csvFile:
+    csvFile.write('product_page_url,universal_ product_code (upc),title,price_including_tax,price_excluding_tax,'
+                  'number_available,product_description,category,review_rating,image_url\n')
 
-    for i in range(2):
-        response = requests.get(pageSite(i))
+    # Boucle sur les 50 pages du site:
+    for i in range(51):
+        response = requests.get(pagesite(i))
 
+        # Conditionnel et définition de l'objet soup sur les pages du site:
         if response.ok:
             soup = BeautifulSoup(response.text, 'html.parser')
-            articles = soup.findAll('h3')
-            for h3 in articles:
-                urlLinks = pageLivre(h3,'a','href')
-                response = requests.get(urlLinks)
 
+            # Boucle sur les pages des livres:
+            for h3 in soup.findAll('h3'):
+                response = requests.get(pagelivre(h3, 'a', 'href'))
+
+                # Conditionnel et définition de l'objet soup sur les pages des livres
                 if response.ok:
-                    soup = BeautifulSoup(response.text,'html.parser')
-                    for i in soup.findAll('th'):
-                        if i.text == 'UPC':
-                            code = i.findNext('td').text
-                        if i.text == 'Price (excl. tax)':
-                            priceBrut = i.findNext('td').text.replace('Â','')
-                        if i.text == 'Price (incl. tax)':
-                            priceNet = i.findNext('td').text.replace('Â','')
-                        if i.text == 'Availability':
-                            stock = re.findall('\d+',(i.findNext('td').text))[0]
-                    title = soup.find('h1').text.replace(',','').replace('/','')
-                    description = soup.findAll('p')[3].text.replace(',','').replace('"','')
+                    soup = BeautifulSoup(response.text, 'html.parser')
+
+                    # Définition des données des en-têtes de colonnes:
+                    urlLinks = pagelivre(h3, 'a', 'href')
+                    title = soup.find('h1').text.replace(',', '').replace('/', '')
+                    description = soup.findAll('p')[3].text.replace(',', '').replace('"', '')
                     category = soup.findAll('a')[3].text
                     review = chiffre(soup.findAll('p')[2]['class'][1])
-                    imageLinks = soup.find('img')['src'].replace('../../','http://books.toscrape.com/')
+                    imageLinks = soup.find('img')['src'].replace('../../', 'http://books.toscrape.com/')
+                    for j in soup.findAll('th'):
+                        if j.text == 'UPC':
+                            code = j.findNext('td').text
+                        if j.text == 'Price (excl. tax)':
+                            priceBrut = j.findNext('td').text.replace('Â', '')
+                        if j.text == 'Price (incl. tax)':
+                            priceNet = j.findNext('td').text.replace('Â', '')
+                        if j.text == 'Availability':
+                            stock = re.findall(r'\d+', j.findNext('td').text)[0]
 
-                    csvFile.write(urlLinks + ',' + code + ',' + title + ',' + priceNet + ',' + priceBrut + ',' + stock + ',' + description + ',' + category + ',' + review + ',' + imageLinks + '\n')
-                    saveImage(imageLinks,title)
+                    # Écriture des données des en-têtes de colonnes dans le fichier data.csv:
+                    csvFile.write(urlLinks + ',' + code + ',' + title + ',' + priceNet + ',' + priceBrut + ','
+                                  + stock + ',' + description + ',' + category + ',' + review + ',' + imageLinks + '\n')
+
+                    # Téléchargement des images dans le dossier export/images/:
+                    saveimage(imageLinks, title)
 
 print("Fin d'extraction des données")
