6370ab54ddc741faf5cdd803bf8b4349776412e7
Martin Giraud <mrtngiraud@gmail.com>
2021-01-15 20:51:52 +0100

    utilisation du paquet csv pour gérer les problématiques d'encodage, allègement du code

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from bs4 import BeautifulSoup
 import os
 import re
+import csv
 
 # Import des fonctions:
 from fonctions import *
@@ -13,17 +14,19 @@ os.mkdir('export/')
 os.mkdir('export/images/')
 
 # Ouverture d'un fichier data.csv et écriture des en-têtes de colonnes:
-with open('export/'+'data.csv', 'a+', newline='') as csvFile:
-    csvFile.write('product_page_url,universal_ product_code (upc),title,price_including_tax,price_excluding_tax,'
-                  'number_available,product_description,category,review_rating,image_url\n')
+with open('export/'+'data.csv', 'a+', newline='', encoding='utf-8-sig') as csvFile:
+    writer = csv.writer(csvFile, dialect='excel')
+    writer.writerow(['product_page_url', 'universal_ product_code (upc)', 'title', 'price_including_tax',
+                     'price_excluding_tax', 'number_available', 'product_description', 'category', 'review_rating',
+                     'image_url'])
 
     # Boucle sur les 50 pages du site:
-    for i in range(51):
+    for i in range(3):
         response = requests.get(pagesite(i))
 
         # Conditionnel et définition de l'objet soup sur les pages du site:
         if response.ok:
-            soup = BeautifulSoup(response.text, 'html.parser')
+            soup = BeautifulSoup(response.content, 'html.parser')
 
             # Boucle sur les pages des livres:
             for h3 in soup.findAll('h3'):
@@ -31,12 +34,12 @@ with open('export/'+'data.csv', 'a+', newline='') as csvFile:
 
                 # Conditionnel et définition de l'objet soup sur les pages des livres
                 if response.ok:
-                    soup = BeautifulSoup(response.text, 'html.parser')
+                    soup = BeautifulSoup(response.content, 'html.parser')
 
                     # Définition des données des en-têtes de colonnes:
                     urlLinks = pagelivre(h3, 'a', 'href')
-                    title = soup.find('h1').text.replace(',', '').replace('/', '')
-                    description = soup.findAll('p')[3].text.replace(',', '').replace('"', '')
+                    title = soup.find('h1').text
+                    description = soup.findAll('p')[3].text
                     category = soup.findAll('a')[3].text
                     review = chiffre(soup.findAll('p')[2]['class'][1])
                     imageLinks = soup.find('img')['src'].replace('../../', 'http://books.toscrape.com/')
@@ -44,15 +47,15 @@ with open('export/'+'data.csv', 'a+', newline='') as csvFile:
                         if j.text == 'UPC':
                             code = j.findNext('td').text
                         if j.text == 'Price (excl. tax)':
-                            priceBrut = j.findNext('td').text.replace('Â', '')
+                            priceBrut = j.findNext('td').text
                         if j.text == 'Price (incl. tax)':
-                            priceNet = j.findNext('td').text.replace('Â', '')
+                            priceNet = j.findNext('td').text
                         if j.text == 'Availability':
                             stock = re.findall(r'\d+', j.findNext('td').text)[0]
 
                     # Écriture des données des en-têtes de colonnes dans le fichier data.csv:
-                    csvFile.write(urlLinks + ',' + code + ',' + title + ',' + priceNet + ',' + priceBrut + ','
-                                  + stock + ',' + description + ',' + category + ',' + review + ',' + imageLinks + '\n')
+                    writer.writerow([urlLinks, code, title, priceNet, priceBrut, stock, description, category, review,
+                                     imageLinks])
 
                     # Téléchargement des images dans le dossier export/images/:
                     saveimage(imageLinks, title)
