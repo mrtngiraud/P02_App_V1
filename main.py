a285b08e4e296742008d3730886ea0091c268ebf
Martin Giraud <mrtngiraud@gmail.com>
2021-01-18 12:30:53 +0100

    correction url pagelivre et nouvelle organisation du dossier export

--- main.py
+++ main.py
@@ -11,30 +11,33 @@ print("Début d'extraction des données")
 
 # Création des dossiers export/ et export/images/:
 os.mkdir('export/')
-os.mkdir('export/images/')
-os.mkdir('export/datas/')
+#os.mkdir('export/images/')
+#os.mkdir('export/datas/')
 
-response = requests.get('http://books.toscrape.com/index.html')
 
-# Conditionnel et définition de l'objet soup sur la page d'accueil du site:
-if response.ok:
-    soup = BeautifulSoup(response.content, 'html.parser')
+# Boucle sur les pages des catégories:
+for i in range(3, 5):
+    response = requests.get('http://books.toscrape.com/index.html')
 
-    # Boucle sur les pages des catégories:
-    for i in range(3, 10):
-        category = soup.findAll('li')[i].text.replace(' ','')
-        response = requests.get(pagecategorie('li', 'a', 'href', i))
+    # Conditionnel et définition de l'objet soup sur la page d'accueil du site:
+    if response.ok:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        categoryhome = soup.findAll('li')[i].text.replace(' ', '')
+        linkcategory = soup.findAll('li')[i].find('a')['href']
+        response = requests.get('http://books.toscrape.com/{}'.format(linkcategory))
 
         # Ouverture d'un fichier data.csv et écriture des en-têtes de colonnes:
-        with open('export/datas/' + category + '.csv', 'a+', newline='', encoding='utf-8-sig') as csvFile:
+        os.mkdir('export/{}/'.format(categoryhome))
+        os.mkdir('export/{}/images/'.format(categoryhome))
+        with open('export/{}/data.csv'.format(categoryhome), 'a+', newline='', encoding='utf-8-sig') as csvFile:
             writer = csv.writer(csvFile, dialect='excel')
             writer.writerow(['product_page_url', 'universal_ product_code (upc)', 'title', 'price_including_tax',
                              'price_excluding_tax', 'number_available', 'product_description', 'category',
                              'review_rating',
                              'image_url'])
+
             if response.ok:
                 soup = BeautifulSoup(response.content, 'html.parser')
-                print('ok')
 
                 # Boucle sur les pages des livres:
                 for h3 in soup.findAll('h3'):
@@ -43,7 +46,6 @@ if response.ok:
                     # Conditionnel et définition de l'objet soup sur les pages des livres
                     if response.ok:
                         soup = BeautifulSoup(response.content, 'html.parser')
-                        print('ok')
 
                         # Définition des données des en-têtes de colonnes:
                         urllivre = pagelivre(h3, 'a', 'href')
@@ -63,13 +65,14 @@ if response.ok:
                                 stock = re.findall(r'\d+', j.findNext('td').text)[0]
 
                         # Écriture des données des en-têtes de colonnes dans le fichier data.csv:
-                        writer.writerow([urllivre, code, title, priceNet, priceBrut, stock, description, category, review,
-                                         urlimage])
-                        print([urllivre, code, title, priceNet, priceBrut, stock, description, category, review,
-                                         urlimage])
+                        writer.writerow([urllivre, code, title, priceNet, priceBrut, stock, description, category,
+                                         review, urlimage])
 
                         # Téléchargement des images dans le dossier export/images/:
-                        saveimage(urlimage, title)
-                        print(urlimage)
+                        #saveimage(urlimage, title)
+                        response = requests.get(urlimage)
+                        if response.ok:
+                            with open('export/{}/images/{}.jpg'.format(categoryhome, title), 'wb') as imageFile:
+                                imageFile.write(response.content)
 
 print("Fin d'extraction des données")
--- scraping_tools.py
+++ scraping_tools.py
@@ -8,14 +8,13 @@ a = balise head de b
 b = 'balise body de c'
 c = 'class de c du lien hypertexte'
 i = itération de 3 à 53
-'''
 def pagecategorie(a, b, c, i):
     response = requests.get('http://books.toscrape.com/index.html')
     if response.ok:
         soup = BeautifulSoup(response.content, 'html.parser')
         link = soup.findAll(a)[i].find(b)[c]
         return 'http://books.toscrape.com/{}'.format(link)
-
+'''
 
 # Fonction2/ Renvoyer l'url d'une page d'un livre:
 '''
@@ -24,8 +23,8 @@ b = 'balise body de c'
 c = 'class de c du lien hypertexte'
 '''
 def pagelivre(a, b, c):
-    link = a.find(b)[c]
-    return 'http://books.toscrape.com/catalogue/{}'.format(link)
+    linklivre = a.find(b)[c]
+    return linklivre.replace('../../../', 'http://books.toscrape.com/catalogue/')
 
 # Fonction3/ Convertir nombre lettre en nombre chiffre (de 1 à 5):
 '''
