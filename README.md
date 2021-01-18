bf8f45b4a60e337c33c43560a28c6cfb7c01316d
Martin Giraud <mrtngiraud@gmail.com>
2021-01-18 10:50:52 +0100

    test fonction pagecategorie

--- README.md
+++ README.md
@@ -24,7 +24,75 @@ Pour cela, un script Python a été développé, mettant principalement en oeuvr
 ***
 #### 2. Démarrage
 
-Afin d'exécuter le script correctement, il est recommandé de créer un environnement virtuel et d'y installer les paquets Python requis en suivant les instructions du fichier **requirements.txt**.
+Afin d'exécuter le script (main.py) correctement, il est recommandé, dans l'ordre :
+
+######2.1 d'avoir déjà installé Python dans une version >= 3.4
+
+Dans une ligne de terminal, écrire :
+
+        python3 --version
+
+Si vous recevez un message d'erreur indiquant que Python est introuvable,
+ou un message similaire, il est probable que votre installation de Python
+soit incorrecte ou manquante. Dans ce cas, vous devez télécharger et installer
+la dernière version de Python avant de continuer.
+
+Sinon vous devrier voir s'afficher Python 3.4...
+
+######2.3 de créer un environnement virtuel à la racine du dossier projet
+
+Dans une ligne de terminal, viser le dossier et écrire :
+
+        python3 -m venv env
+
+######2.4 d'activer l'environnement virtuel créé précédemment
+
+Dans une ligne de terminal, écrire:
+
+        source env/bin/activate
+
+######2.5 d'installer les paquets Python suivants dans l'environnement virtuel activé précédemment
+
+Dans une ligne de terminal, écrire :
+
+        pip3 install beautifulsoup4
+        pip3 install requests
+
+Pour plus d'informations, consulter PyPi.org.
+
+######2.6 de vérifier la bonne installation des paquets Python
+
+Dans une ligne de terminal, taper :
+
+        pip3 freeze
+
+Vous devriez obtenir :
+
+        beautifulsoup4==4.9.3
+        bs4==0.0.1
+        certifi==2020.12.5
+        chardet==4.0.0
+        idna==2.10
+        requests==2.25.1
+        soupsieve==2.1
+        urllib3==1.26.2
+
+######6. d'exécuter le script
+
+Dans une ligne de terminal, taper :
+        
+        python3 main.py
+
+Si, pour une raison, vous devez relancer l'exécution du script,
+veillez à bien supprimer le dossier export généré auparavant,
+sinon le message d'erreur ci-dessous s'affiche :
+
+        Traceback (most recent call last):
+        File "main.py", line 5, in <module>
+        os.mkdir('export/')
+        FileExistsError: [Errno 17] File exists: 'export/'
+
+######7. d'attendre la fin de l'exécution du script avant de consulter les données "scrapées" dans le dossier export/ généré
 
 ***
 #### 3. Auteur
--- main.py
+++ main.py
@@ -5,59 +5,71 @@ import re
 import csv
 
 # Import des fonctions:
-from fonctions import *
+from scraping_tools import *
 
 print("Début d'extraction des données")
 
 # Création des dossiers export/ et export/images/:
 os.mkdir('export/')
 os.mkdir('export/images/')
+os.mkdir('export/datas/')
 
-# Ouverture d'un fichier data.csv et écriture des en-têtes de colonnes:
-with open('export/'+'data.csv', 'a+', newline='', encoding='utf-8-sig') as csvFile:
-    writer = csv.writer(csvFile, dialect='excel')
-    writer.writerow(['product_page_url', 'universal_ product_code (upc)', 'title', 'price_including_tax',
-                     'price_excluding_tax', 'number_available', 'product_description', 'category', 'review_rating',
-                     'image_url'])
+response = requests.get('http://books.toscrape.com/index.html')
 
-    # Boucle sur les 50 pages du site:
-    for i in range(51):
-        response = requests.get(pagesite(i))
+# Conditionnel et définition de l'objet soup sur la page d'accueil du site:
+if response.ok:
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-        # Conditionnel et définition de l'objet soup sur les pages du site:
-        if response.ok:
-            soup = BeautifulSoup(response.content, 'html.parser')
+    # Boucle sur les pages des catégories:
+    for i in range(3, 10):
+        category = soup.findAll('li')[i].text.replace(' ','')
+        response = requests.get(pagecategorie('li', 'a', 'href', i))
 
-            # Boucle sur les pages des livres:
-            for h3 in soup.findAll('h3'):
-                response = requests.get(pagelivre(h3, 'a', 'href'))
+        # Ouverture d'un fichier data.csv et écriture des en-têtes de colonnes:
+        with open('export/datas/' + category + '.csv', 'a+', newline='', encoding='utf-8-sig') as csvFile:
+            writer = csv.writer(csvFile, dialect='excel')
+            writer.writerow(['product_page_url', 'universal_ product_code (upc)', 'title', 'price_including_tax',
+                             'price_excluding_tax', 'number_available', 'product_description', 'category',
+                             'review_rating',
+                             'image_url'])
+            if response.ok:
+                soup = BeautifulSoup(response.content, 'html.parser')
+                print('ok')
 
-                # Conditionnel et définition de l'objet soup sur les pages des livres
-                if response.ok:
-                    soup = BeautifulSoup(response.content, 'html.parser')
+                # Boucle sur les pages des livres:
+                for h3 in soup.findAll('h3'):
+                    response = requests.get(pagelivre(h3, 'a', 'href'))
 
-                    # Définition des données des en-têtes de colonnes:
-                    urlLinks = pagelivre(h3, 'a', 'href')
-                    title = soup.find('h1').text
-                    description = soup.findAll('p')[3].text
-                    category = soup.findAll('a')[3].text
-                    review = chiffre(soup.findAll('p')[2]['class'][1])
-                    imageLinks = soup.find('img')['src'].replace('../../', 'http://books.toscrape.com/')
-                    for j in soup.findAll('th'):
-                        if j.text == 'UPC':
-                            code = j.findNext('td').text
-                        if j.text == 'Price (excl. tax)':
-                            priceBrut = j.findNext('td').text
-                        if j.text == 'Price (incl. tax)':
-                            priceNet = j.findNext('td').text
-                        if j.text == 'Availability':
-                            stock = re.findall(r'\d+', j.findNext('td').text)[0]
+                    # Conditionnel et définition de l'objet soup sur les pages des livres
+                    if response.ok:
+                        soup = BeautifulSoup(response.content, 'html.parser')
+                        print('ok')
 
-                    # Écriture des données des en-têtes de colonnes dans le fichier data.csv:
-                    writer.writerow([urlLinks, code, title, priceNet, priceBrut, stock, description, category, review,
-                                     imageLinks])
+                        # Définition des données des en-têtes de colonnes:
+                        urllivre = pagelivre(h3, 'a', 'href')
+                        title = soup.find('h1').text
+                        description = soup.findAll('p')[3].text
+                        category = soup.findAll('a')[3].text
+                        review = chiffre(soup.findAll('p')[2]['class'][1])
+                        urlimage = soup.find('img')['src'].replace('../../', 'http://books.toscrape.com/')
+                        for j in soup.findAll('th'):
+                            if j.text == 'UPC':
+                                code = j.findNext('td').text
+                            if j.text == 'Price (excl. tax)':
+                                priceBrut = j.findNext('td').text
+                            if j.text == 'Price (incl. tax)':
+                                priceNet = j.findNext('td').text
+                            if j.text == 'Availability':
+                                stock = re.findall(r'\d+', j.findNext('td').text)[0]
 
-                    # Téléchargement des images dans le dossier export/images/:
-                    saveimage(imageLinks, title)
+                        # Écriture des données des en-têtes de colonnes dans le fichier data.csv:
+                        writer.writerow([urllivre, code, title, priceNet, priceBrut, stock, description, category, review,
+                                         urlimage])
+                        print([urllivre, code, title, priceNet, priceBrut, stock, description, category, review,
+                                         urlimage])
+
+                        # Téléchargement des images dans le dossier export/images/:
+                        saveimage(urlimage, title)
+                        print(urlimage)
 
 print("Fin d'extraction des données")
