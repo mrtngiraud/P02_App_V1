b2d628fd9ba708f5f393517f101d06893973dca2
Martin Giraud <mrtngiraud@gmail.com>
2021-01-20 11:31:18 +0100

    Correction des noms des variables et des doubles quotes des docstrings

--- main.py
+++ main.py
@@ -20,26 +20,25 @@ for i in range(3, 53):
     # Conditionnel et définition de l'objet soup sur la page accueil du site:
     if response.ok:
         soup = BeautifulSoup(response.content, 'html.parser')
-        categoryhome = soup.findAll('li')[i].get_text("\n", strip=True)
-        linkcategory = soup.findAll('li')[i].find('a')['href']
-        response = requests.get('http://books.toscrape.com/{}'.format(linkcategory))
+        category_home = soup.findAll('li')[i].get_text("\n", strip=True)
+        link_category = soup.findAll('li')[i].find('a')['href']
+        response = requests.get('http://books.toscrape.com/{}'.format(link_category))
 
         # Création des dossiers et fichiers d'extraction des données et images:
-        os.mkdir('export/{}/'.format(categoryhome))
-        os.mkdir('export/{}/images/'.format(categoryhome))
-        with open('export/{}/data.csv'.format(categoryhome), 'a+', newline='', encoding='utf-8-sig') as csvFile:
-            writer = csv.writer(csvFile, dialect='excel')
+        os.mkdir('export/{}/'.format(category_home))
+        os.mkdir('export/{}/images/'.format(category_home))
+        with open('export/{}/data.csv'.format(category_home), 'a+', newline='', encoding='utf-8-sig') as csv_file:
+            writer = csv.writer(csv_file, dialect='excel')
             writer.writerow(['product_page_url', 'universal_ product_code (upc)', 'title', 'price_including_tax',
                              'price_excluding_tax', 'number_available', 'product_description', 'category',
-                             'review_rating',
-                             'image_url'])
+                             'review_rating', 'image_url'])
 
             # Enregistrement des données et images des pages catégorie "index"
             if response.ok:
-                save_data_and_image(response, writer, categoryhome)
+                save_data_and_image(response, writer, category_home)
 
             # Boucle sur les pages catégorie 'next'
-            response = requests.get('http://books.toscrape.com/{}'.format(linkcategory))
+            response = requests.get('http://books.toscrape.com/{}'.format(link_category))
             
             if response.ok:
                 soup = BeautifulSoup(response.content, 'html.parser')
@@ -47,11 +46,11 @@ for i in range(3, 53):
                 result = int(str(soup.find('form').find('strong').text))
                 if result > 20:
                     for k in range(2, ceil(result // 20)+2):
-                        linkcategorynext = linkcategory.replace('index', 'page-{}'.format(k))
-                        response = requests.get('http://books.toscrape.com/{}'.format(linkcategorynext))
+                        link_category_next = link_category.replace('index', 'page-{}'.format(k))
+                        response = requests.get('http://books.toscrape.com/{}'.format(link_category_next))
 
                         # Enregistrement des données et images des pages catégorie "next"
                         if response.ok:
-                            save_data_and_image(response, writer, categoryhome)
+                            save_data_and_image(response, writer, category_home)
 
 print("Fin d'extraction des données")
--- scraping_tools.py
+++ scraping_tools.py
@@ -5,23 +5,23 @@ import re
 
 
 def get_digit_from_word(number):
-    '''
+    """
     Convertir nombre lettre en nombre chiffre (de 1 à 5)
     :param number: nombre lettre
     :return: nombre chiffre
-    '''
+    """
     dico = {'One': '1', 'Two': '2', 'Three': '3', 'Four': '4', 'Five': '5'}
     return dico.get(number)
 
 
-def save_data_and_image(response, writer, categoryhome):
-    '''
+def save_data_and_image(response, writer, category_home):
+    """
     Enregistrer les données et images des pages livre
     :param response: résultat de requests.get sur l'url des pages catégorie
     :param writer: objet de csv.writer permettant d'écrire dans le fichier data.csv
-    :param categoryhome: résultat de soup.find sur les catégories de la page d'accueil
+    :param category_home: résultat de soup.find sur les catégories de la page d'accueil
     :return: writer.writerow des données scrapées et imageFile.write des images
-    '''
+    """
     soup = BeautifulSoup(response.content, 'html.parser')
 
     # Boucle sur les pages des livres:
@@ -33,28 +33,28 @@ def save_data_and_image(response, writer, categoryhome):
             soup = BeautifulSoup(response.content, 'html.parser')
 
             # Définition des données des en-têtes de colonnes:
-            urllivre = h3.find('a')['href'].replace('../../../', 'http://books.toscrape.com/catalogue/')
+            url_book = h3.find('a')['href'].replace('../../../', 'http://books.toscrape.com/catalogue/')
             title = soup.find('h1').text
             description = soup.findAll('p')[3].text
             category = soup.findAll('a')[3].text
             review = get_digit_from_word(soup.findAll('p')[2]['class'][1])
-            urlimage = soup.find('img')['src'].replace('../../', 'http://books.toscrape.com/')
+            url_image = soup.find('img')['src'].replace('../../', 'http://books.toscrape.com/')
             for j in soup.findAll('th'):
                 if j.text == 'UPC':
                     code = j.find_next('td').text
                 if j.text == 'Price (excl. tax)':
-                    priceBrut = j.find_next('td').text
+                    price_brut = j.find_next('td').text
                 if j.text == 'Price (incl. tax)':
-                    priceNet = j.find_next('td').text
+                    price_net = j.find_next('td').text
                 if j.text == 'Availability':
                     stock = re.findall(r'\d+', j.find_next('td').text)[0]
 
             # Écriture des données des en-têtes de colonnes dans le fichier data.csv:
-            writer.writerow([urllivre, code, title, priceNet, priceBrut, stock, description, category,
-                             review, urlimage])
+            writer.writerow([url_book, code, title, price_net, price_brut, stock, description, category,
+                             review, url_image])
 
             # Téléchargement des images dans le dossier export/images/:
-            response = requests.get(urlimage)
+            response = requests.get(url_image)
             if response.ok:
-                with open('export/{}/images/{}.jpg'.format(categoryhome, code), 'wb') as imageFile:
-                    imageFile.write(response.content)
+                with open('export/{}/images/{}.jpg'.format(category_home, code), 'wb') as image_file:
+                    image_file.write(response.content)
